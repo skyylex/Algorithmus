2880aecb27238d279288ed8e8f3f060bfed07bba
Yury Lapitsky <skyylex@skyylex.local>
2015-08-01 21:56:40 +0300

    Initial Commit

---
+++ Examples/Stack/Stack/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Stack
+//
+//  Created by Yury Lapitsky on 01.08.15.
+//  Copyright (c) 2015 skyylex. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
